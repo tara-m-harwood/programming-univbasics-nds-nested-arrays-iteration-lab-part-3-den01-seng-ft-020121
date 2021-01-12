4cdb4ba336326293d5d2254e81638dbf17f45857
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 04:33:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 
-total = 0
+joined_string = ''
 row_index = 0
 while row_index < guessing_game_grid.count do
   element_index = 0
