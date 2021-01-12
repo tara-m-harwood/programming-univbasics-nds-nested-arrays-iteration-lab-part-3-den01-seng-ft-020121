aabda8717620fca82170324945628912e1b71de9
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 04:52:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,7 @@ while row_index < src.count do
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a? String
       joined_string += src[row_index][element_index]
+    end
     element_index += 1
   end
   row_index += 1
