339cbf583654f8536f4e897c35c9986c5c87f892
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-12 04:41:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    joined_string += src[row_index][element_index]
+    joined_string += src[row_index][element_index].to_s
     element_index += 1
   end
   row_index += 1
